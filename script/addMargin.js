14926702a543c2c3e304d86d5f2825bfb3d77ef0
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2022-06-17 17:36:51 +0900

    make it compact

--- script/addMargin.js
+++ script/addMargin.js
@@ -2,9 +2,9 @@ let sections = document.querySelectorAll('.section');
 
 sections.forEach((section) => {
   let marginL = document.createElement('div');
-  marginL.className = 'margin margin__l';
+  marginL.className = 'margin margin--left';
   section.prepend(marginL);
   let marginR = document.createElement('div');
-  marginR.className = 'margin margin__r';
+  marginR.className = 'margin margin--right';
   section.append(marginR);
 });

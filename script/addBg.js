271e207d99645379bc277e2e108ad6edb6c98bf8
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2022-07-19 06:11:44 +0900

    easy template done

--- script/addBg.js
+++ script/addBg.js
@@ -1,10 +1,10 @@
 let sections = document.querySelectorAll('.section');
 
 sections.forEach((section) => {
+  let bgR = document.createElement('div');
+  bgR.className = 'section__bg section__bg--right';
+  section.prepend(bgR);
   let bgL = document.createElement('div');
   bgL.className = 'section__bg section__bg--left';
   section.prepend(bgL);
-  let bgR = document.createElement('div');
-  bgR.className = 'section__bg section__bg--right';
-  section.append(bgR);
 });
